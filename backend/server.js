9bb62473a2d2125edc309794fb0b0651aa175e1e
Fussel1003 <165007146+Fussel1003@users.noreply.github.com>
2025-01-07 10:53:56 +0100

    ..

--- backend/server.js
+++ backend/server.js
@@ -128,9 +128,8 @@ app.post('/api/payment-success', (req, res) => {
       }
       console.log("ipOutput: " + ipOutput)
 console.log("user_id: " + userId)
-console.log("instance_id: " + instance_id)
 console.log("slots: " + slots)
-      const serverIP = ipOutput.trim();
+      const serverIP = ipOutput;
       const insertQuery = `
         INSERT INTO servers (user_id, instance_id, slots, status, created_at)
         VALUES (?, ?, ?, 'running', NOW())`;
